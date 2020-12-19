8656e7e93cb4c28eb14da3dc167ce3cc8db127c3
DSmmartin <mjmm_manuel@hotmail.com>
2020-12-19 01:01:47 +0100

    add insertion_sorting

--- problems/sorting.py
+++ problems/sorting.py
@@ -3,7 +3,22 @@ Problem: Sorting
 Input: A sequence of n keys a1,...,an.
 Output: The permutation (reordering) of the input sequence such that:
 a'1 <= a'2 <= ... <= a'n-1 <= a'n
+
+Note that we seek algorithms taht are correct and efficient, while being easy to implement. But, may not be simultaneously achievable.
 """
 
-def sorting(sequence: list) -> list:
+def insertion_sort(sequence: list[int]) -> list[int]:
+    """Notes:
+    1. simple
+    2. efficient in short Vs not-efficient in larger lists
+    3. adaptive
+    4. stable
+    5. in-place
+    6. online
+    """
+    for i in range(len(sequence)):
+        j = i
+        while ((j > 0) & (sequence[j] < sequence[j - 1])):
+            sequence[j], sequence[j-1] = sequence[j-1], sequence[j]
+            j -= 1
     return sequence
--- test/test_sorting.py
+++ test/test_sorting.py
@@ -1,11 +1,11 @@
 import unittest
 
-from problems.sorting import sorting
+from problems.sorting import insertion_sort
 
 class TestSortign(unittest.TestCase):
     def test_sorting(self):
         data = [9, 4, 5, 6, 4]
-        result = sorting(data)
+        result = insertion_sort(data)
         self.assertListEqual(result, [4, 4, 5, 6, 9])
 
 if __name__ == '__main__':
